61b3e68605dce32a1d8e13cadf010e428170e8cb
ahmetyasiir <ahmetyasiraydn@gmail.com>
2021-12-14 19:43:39 +0300

    Initial Commit

---
+++ HF1-Patika-Mobven/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HF1-Patika-Mobven
+//
+//  Created by Ahmet Yasir Aydın on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
